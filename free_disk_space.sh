634ef17f2c6cc385b9a3cf8312b34936bbfa9649
Diego Molina <diemol@gmail.com>
2023-02-22 14:36:56 +0000

    Try out

--- free_disk_space.sh
+++ free_disk_space.sh
@@ -32,16 +32,16 @@ echo "==========================================================================
 echo "Listing 100 largest packages"
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 df -h
-# echo "Removing large packages"
-# sudo apt-get remove -y '^dotnet-.*'
-# sudo apt-get remove -y '^llvm-.*'
-# sudo apt-get remove -y 'php.*'
-# sudo apt-get remove -y '^mongodb-.*'
-# sudo apt-get remove -y '^mysql-.*'
-# sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri
-# sudo apt-get autoremove -y
-# sudo apt-get clean
-# df -h
+echo "Removing large packages"
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y '^mongodb-.*'
+sudo apt-get remove -y '^mysql-.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk hhvm powershell mono-devel libgl1-mesa-dri
+sudo apt-get autoremove -y
+sudo apt-get clean
+df -h
 # echo "Removing large directories"
 
 # sudo rm -rf /usr/share/dotnet/
